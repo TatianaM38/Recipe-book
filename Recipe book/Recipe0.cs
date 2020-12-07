8109d8ab9a746f9ebe78a5e65c8740472d1fda6b
tmichalikova231@gmail.com <tmichalikova231@gmail.com>
2020-12-07 12:58:02 +0100

    Add rating to recipes

--- Recipe book/Recipe0.cs
+++ Recipe book/Recipe0.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Xml;
+using System.IO;
+
 
 namespace Recipe_book
 {
@@ -16,7 +18,7 @@ namespace Recipe_book
                 {
                     Console.ForegroundColor = ConsoleColor.DarkCyan;
                     string s1 = xml0.ReadElementString();
-                    Console.WriteLine("                                           " + s1);                  
+                    Console.WriteLine("                                           " + s1);
                 }
                 if (xml0.NodeType == XmlNodeType.Element && xml0.Name == "date")
                 {
@@ -46,16 +48,49 @@ namespace Recipe_book
                     string s6 = xml0.ReadElementString();
                     Console.WriteLine("Autor receptu: " + s6);
                 }
-                if (xml0.NodeType == XmlNodeType.Element && xml0.Name == "rating")
-                {
-                    Console.WriteLine("");
-                    string s7 = xml0.ReadElementString();
-                    Console.WriteLine("Ohodnoťte recept: " + s7);
-                }
+
+
             }
+
+            Console.ReadLine();
+            int avg;
+            string filename = ("C:\\Users\\Administrátor\\Desktop\\R.xml");
+            string[] readText = File.ReadAllLines(filename);
+            string[] write = new string[1];
+            Console.WriteLine("Ohodnoťte recept 1-5 : " );
+            int str = Convert.ToInt32(Console.ReadLine());
+            int old = Convert.ToInt32(readText[0]);
+            avg = (str + old) / 2;
+            write[0] = avg.ToString();
+            File.WriteAllLines(filename, write);
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            string s = @"
+
+                  .────────────────────────────────────────────────────────────────.
+                  |                      [S]  Späť na recepty                      |
+                  *────────────────────────────────────────────────────────────────* ";
+            Console.WriteLine(s);
+            Console.ReadLine();
+
+            char button1 = Convert.ToChar(Console.Read());
+            string buttonString1 = button1.ToString();
+            if (buttonString1 == "S")
+            {
+                Console.Clear();
+                Menu hlavna = new Menu();
+                hlavna.DesignMenu();
+            }
+            else if (buttonString1 == "s")
+            {
+                Console.Clear();
+                Menu hlavna = new Menu();
+                hlavna.DesignMenu();
+            }
+
+
         }
-        
 
-
+         
     }
 }
--- Recipe book/Recipe2.cs
+++ Recipe book/Recipe2.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Xml;
 
@@ -52,6 +53,43 @@ namespace Recipe_book
                     string s7 = xml1.ReadElementString();
                     Console.WriteLine("Ohodnoťte recept: " + s7);
                 }
+            }
+
+
+            Console.ReadLine();
+            int avg;
+            string filename = ("C:\\Users\\Administrátor\\Desktop\\R.xml");
+            string[] readText = File.ReadAllLines(filename);
+            string[] write = new string[1];
+            Console.WriteLine("Ohodnoťte recept 1-5 : ");
+            int str = Convert.ToInt32(Console.ReadLine());
+            int old = Convert.ToInt32(readText[0]);
+            avg = (str + old) / 2;
+            write[0] = avg.ToString();
+            File.WriteAllLines(filename, write);
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            string s = @"
+
+                  .────────────────────────────────────────────────────────────────.
+                  |                      [S]  Späť na recepty                      |
+                  *────────────────────────────────────────────────────────────────* ";
+            Console.WriteLine(s);
+            Console.ReadLine();
+
+            char button1 = Convert.ToChar(Console.Read());
+            string buttonString1 = button1.ToString();
+            if (buttonString1 == "S")
+            {
+                Console.Clear();
+                Menu hlavna = new Menu();
+                hlavna.DesignMenu();
+            }
+            else if (buttonString1 == "s")
+            {
+                Console.Clear();
+                Menu hlavna = new Menu();
+                hlavna.DesignMenu();
             }
         }
     }
